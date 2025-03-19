094b0ab2ecd1c2a0d074022704bca14f68dac0c4
zarakikenpashi <sadiarog.abel@gmail.com>
2025-03-19 01:50:35 +0000

    sidebar start

--- src/components/Sidebar/Siderbar.tsx
+++ src/components/Sidebar/Siderbar.tsx
@@ -1,6 +1,126 @@
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoCar } from "react-icons/io5";
+import { IoChevronDownOutline, IoHomeOutline } from "react-icons/io5"
+import { GoAlert } from "react-icons/go";
+import { CiClock2, CiSettings } from "react-icons/ci";
+import { LiaKeySolid } from "react-icons/lia";
+import { BsFileEarmarkPdf, BsPeople } from "react-icons/bs";
+import { LuBox, LuFuel } from "react-icons/lu";
+import { FiSidebar } from "react-icons/fi";
+import { MdOutlinePlace } from "react-icons/md";
+
 const Siderbar = () => {
   return (
-    <div className="bg-gray">Siderbar</div>
+    <div className="bg-gray">
+      <div className="flex flex-col justify-between h-full py-2 pr-2 pl-0.5">
+        <h1 className="text-4xl font-black px-6">
+          <span className="text-purple">my</span>
+          Logo
+        </h1>
+        
+        <nav className="flex  flex-col gap-y-2">
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <IoHomeOutline className="text-sm" />
+              <span>Dashboard</span>
+            </span>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6 bg-light text-dark font-groteskMD py-1 border-s-2 rounded-r-md">
+            <span className="flex items-center gap-x-2 text-sm">
+              <IoCar className="text-sm" />
+              <span>Vehicules</span>
+            </span>
+            <IoChevronDownOutline  className="text-sm"/>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6 py-1 border-s-2 border-gray hover:bg-light  hover:border-dark hover:rounded-r-md hover:text-dark hover:font-groteskMD">
+            <span className="flex items-center gap-x-2 text-sm">
+              <IoCar  className="text-sm" />
+              <span>Equipement</span>
+            </span>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <FaRegUserCircle className="text-sm" />
+              <span>Inspections</span>
+            </span>
+            <IoChevronDownOutline  className="text-sm"/>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <GoAlert />
+              <span>Issues</span>
+            </span>
+            <IoChevronDownOutline  className="text-sm"/>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <CiClock2 />
+              <span>Reminders</span>
+            </span>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <LiaKeySolid className="text-sm" />
+              <span>Services</span>
+            </span>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <BsPeople className="text-sm" />
+              <span>Contacts</span>
+            </span>
+            <IoChevronDownOutline  className="text-sm"/>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <LuBox className="text-sm" />
+              <span>Vendors</span>
+            </span>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <FiSidebar className="text-sm" />
+              <span>Parts</span>
+            </span>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <LuFuel className="text-sm" />
+              <span>Fuel History</span>
+            </span>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <MdOutlinePlace className="text-sm" />
+              <span>Places</span>
+            </span>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <BsFileEarmarkPdf className="text-sm" />
+              <span>Documents</span>
+            </span>
+          </a>
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <BsFileEarmarkPdf className="text-sm" />
+              <span>Reports</span>
+            </span>
+          </a>
+        </nav>
+        
+        <div className="">
+          <a href="#" className="flex justify-between items-center px-6">
+            <span className="flex items-center gap-x-2 text-sm">
+              <CiSettings className="text-sm" />
+              <span>Settings</span>
+            </span>
+            <IoChevronDownOutline  className="text-sm"/>
+          </a>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- src/components/Workspace/Topbar.tsx
+++ src/components/Workspace/Topbar.tsx
@@ -1,27 +1,35 @@
 import { IoIosNotificationsOutline } from "react-icons/io";
-import { CiLight, CiDark } from "react-icons/ci";
+import { CiLight } from "react-icons/ci";
 import { FiCommand, FiSearch } from "react-icons/fi";
 import { IoChevronDownOutline } from "react-icons/io5";
+import { useState } from "react";
 const Topbar = () => {
+    const [open, setOpen] = useState(false)
   return (
     <div className="border-b-2 border-gray px-4 py-2  justify-between items-center text-dark hidden lg:flex">
-        <button className="rounded shadow border-gray flex items-center justify-between gap-x-2 py-1 px-2 xl:w-[500px]">
+        <button className="rounded border-2 border-gray flex items-center justify-between gap-x-2 py-1 px-2 lg:w-[400px] xl:w-[500px]">
             <FiSearch className="text-lg" />
-            <input type="text" placeholder="Search..." className="w-full bg-transparent placeholder:text-dark focus:outline-none" />
+            <input type="text" placeholder="Search..." className="w-full bg-transparent placeholder:font-bold placeholder:text-sm focus:outline-none" />
             <span className="p-1 flex gap-0.5 items-center shadow bg-stone-50 rounded text-dark text-sm">
                 <FiCommand />K
             </span>
         </button>
-        <div className="flex items-center gap-x-4">
+        <div className="flex items-center gap-x-4 relative">
             <button className="text-lg cursor-pointer"><IoIosNotificationsOutline /></button>
             <button className="text-lg cursor-pointer">
                 <CiLight />
             </button>
-            <button className="flex items-center gap-x-1 cursor-pointer border-2 border-gray px-2 py-1 rounded-md">
+            <button 
+                className="flex items-center gap-x-1 cursor-pointer border-2 border-gray px-2 py-1 rounded-md"
+                onClick={()=>setOpen(!open)}
+            >
                 <span className="bg-[#ffD980] rounded-full size-4 flex"><img src="https://api.dicebear.com/9.x/notionists/svg" /></span>
                 <div className="text-sm">Washim Showludury</div>
                 <IoChevronDownOutline  className="text-sm"/>
             </button>
+            <div className={`absolute  text-sm right-0 top-8 bg-gray px-1.5 py-2 ${open ? 'block' : 'hidden'}`}>
+                Lorem ipsum dolor sit amet
+            </div>
         </div>
     </div>
   )
